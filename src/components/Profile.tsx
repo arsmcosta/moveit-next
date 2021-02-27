e7df32426326785ae5a060a183b6e3801d85e6d3
Renato Costa <arsmcosta@gmail.com>
2021-02-27 01:36:25 -0300

    Dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,18 @@
-import styles from '../styles/components/Profile.module.css';
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
-    return(
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/arsmcosta.png" alt="Renato Costa"/>
-            <div>
-                <p><img src="icons/level.svg" alt="Level"/>
-                Level 1
-                </p>
-            </div>
-        </div>
-
-    );
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/arsmcosta.png" alt="Renato Costa" />
+      <div>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
